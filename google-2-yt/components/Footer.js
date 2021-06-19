430df80186306429f33ba72acdc46d28c8d58858
Swaroop <swaroop.chakraborty@cerner.com>
2021-06-19 08:09:08 -0400

    commit fotter

--- google-2-yt/components/Footer.js
+++ google-2-yt/components/Footer.js
@@ -1,4 +1,4 @@
-import { GlobeIcon } from "@heroicons/react/solid"
+import { GlobeIcon, HeartIcon } from "@heroicons/react/solid"
 function Footer() {
     return (
         <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100">
@@ -16,8 +16,7 @@ function Footer() {
                     <p className="link">How Search works</p>
                 </div>
                 <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 ">
-                    <GlobeIcon className="h-5 mr-1 text-green-700" />
-                    Carbon neutral since 2007
+                    Made with&nbsp;<HeartIcon className="h-5 mr-1 text-red-700" /> by &nbsp;<a className="hover:underline text-blue-400" href="#https://github.com/ayanchax?tab=repositories" target="_blank">Ayan Chax</a>
                 </div>
                 <div className="flex justify-center space-x-8 mr-4  md:ml-auto text-gray-500">
                     <p className="link">Privacy</p>
