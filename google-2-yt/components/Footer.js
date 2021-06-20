805e4677d9e255d260bfde051ad3fc56c1450527
Swaroop <swaroop.chakraborty@cerner.com>
2021-06-20 13:49:13 -0400

    commit

--- google-2-yt/components/Footer.js
+++ google-2-yt/components/Footer.js
@@ -1,14 +1,11 @@
-import { GlobeIcon, HeartIcon } from "@heroicons/react/solid"
+import { GlobeIcon, HeartIcon } from "@heroicons/react/solid";
 function Footer() {
     return (
         <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100">
             <div className="px-8 py-3 text-gray-500 ml-3">
-                <p>
-                    India
-                </p>
+                <p>India</p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3  ">
-
                 <div className="flex justify-center space-x-8 ml-3  whitespace-nowrap md:justify-self-start text-gray-500">
                     <p className="link">About</p>
                     <p className="link">Advertising</p>
@@ -16,7 +13,14 @@ function Footer() {
                     <p className="link">How Search works</p>
                 </div>
                 <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 ">
-                    Made with&nbsp;<HeartIcon className="h-5 mr-1 text-red-700" /> by &nbsp;<a className="hover:underline text-blue-400" href="https://github.com/ayanchax?tab=repositories" target="_blank">Ayan Chax</a>
+                    Made with&nbsp;
+                    <HeartIcon className="h-5 mr-1 text-red-700" /> by &nbsp;
+                    <a className="hover:underline text-blue-400"
+                        href="https://github.com/ayanchax?tab=repositories"
+                        target="_blank"
+                        rel="noreferrer">
+                        Ayan Chax
+                    </a>
                 </div>
                 <div className="flex justify-center space-x-8 mr-4  md:ml-auto text-gray-500">
                     <p className="link">Privacy</p>
@@ -25,7 +29,7 @@ function Footer() {
                 </div>
             </div>
         </footer>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
